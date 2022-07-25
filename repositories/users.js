0703b2b16ff353c7e2aeaba350707f8729ca948c
Paweł Szczygieł <pawel86szczygiel@gmail.com>
2022-07-25 17:54:20 +0200

    html template functions

--- repositories/users.js
+++ repositories/users.js
@@ -44,6 +44,15 @@ class UsersRepository {
     return record;
   }
 
+  async comparePasswords(saved, supplied) {
+    // Saved -> password saved in our database. 'hashed.salt'
+    // Supplied -> password given to us by a user trying sign in
+    const [hashed, salt] = saved.split('.');
+    const hashedSuppliedBuf = await scrypt(supplied, salt, 64);
+
+    return hashed === hashedSuppliedBuf.toString('hex');
+  }
+
   async writeAll(records) {
     await fs.promises.writeFile(
       this.filename,
